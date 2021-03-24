929162bf6d1f98faaff065b026a3ee30ba004e73
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2021-03-24 14:36:44 -0300

    (4) modificacao para rxjava

--- src/main/kotlin/br/com/zup/bootcamp/orange/Cats.kt
+++ src/main/kotlin/br/com/zup/bootcamp/orange/Cats.kt
@@ -7,30 +7,45 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
+import io.micronaut.scheduling.TaskExecutors
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import org.slf4j.LoggerFactory
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Controller("/cats")
-class CatResource(val cats: Cats) {
+class CatResource(@Named(TaskExecutors.IO) val ioExecutor: ExecutorService, val cats: Cats) {
+
+    val ioScheduler = Schedulers.from(ioExecutor)
 
     val log = LoggerFactory.getLogger(CatResource::class.java)
 
     @Post
     fun create(@Body request: CreateNewCatRequest) =
             request.also { log.info("the thread starting is ${Thread.currentThread().name}") }
-                    .toCat()
-                    .let { cats.save(it) }
-                    .also { log.info("the thread ending is ${Thread.currentThread().name}") }
+                    .let { Single.just(it)
+                                .doOnSubscribe { log.info("the thread starting is ${Thread.currentThread().name}") }
+                                .subscribeOn(ioScheduler)
+                                .map { it.toCat() }
+                                .map { cats.save(it) }
+                                .doOnTerminate { log.info("the thread ending is ${java.lang.Thread.currentThread().name}") }}
 
     @Get("/{id}")
     fun get(@PathVariable id: Int) =
             id.also { log.info("the thread starting is ${Thread.currentThread().name}") }
-                .let { cats.findById(id) }
-                .let { Thread.sleep(1000) }
-                .also { log.info("the thread ending is ${Thread.currentThread().name}") }
+                .let { Single.just(it)
+                            .doOnSubscribe { log.info("the thread starting is ${Thread.currentThread().name}") }
+                            .subscribeOn(ioScheduler)
+                            .map { cats.findById(it) }
+                            .delay(1000, TimeUnit.MILLISECONDS)
+                            .observeOn(ioScheduler)
+                            .doOnTerminate { log.info("the thread ending is ${Thread.currentThread().name}") }}
 }
 
 data class CreateNewCatRequest(val name: String, val age: Int) {
